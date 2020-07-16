00b516a278b9b5b14c4a9c9f85a453b31c20fd5c
xuerb <870418012@qq.com>
2020-07-16 09:44:31 +0800

    django学习项目，配置了用户登录、注册，可以下载老师信息的csv文件，可以以图表的形式查看信息。

---
+++ djangotest/urls.py
@@ -0,0 +1,39 @@
+"""djangotest URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from polls import views
+from .settings import DEBUG
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.show_subjects),
+    path('login/', views.login),
+    path('logout/', views.logout),
+    path('teachers/', views.show_teachers),
+    path('praise/', views.praise_or_criticize),
+    path('criticize/', views.praise_or_criticize),
+    path('captcha/', views.get_captcha),
+    path('excel/', views.export_teachers_excel),
+    path('teachers_data/', views.get_teachers_data),
+    path('register/', views.register),
+    path('teacher_data_show/', views.teacher_data_show),
+]
+if DEBUG:
+
+    import debug_toolbar
+
+    urlpatterns.insert(0, path('__debug__/', include(debug_toolbar.urls)))
---
+++ polls/views.py
@@ -0,0 +1,163 @@
+from django.db.models import Avg
+from django.shortcuts import render, redirect
+from .models import Subject, Teacher, User
+from django.http import JsonResponse, HttpResponse, HttpRequest
+from .utils import gen_random_code, Captcha, gen_md5_digest
+import xlwt
+from io import BytesIO
+from urllib.parse import quote
+import hashlib
+# Create your views here.
+
+
+def show_subjects(request):
+    print(request.session)
+    subjects = Subject.objects.all().order_by('no')
+    return render(request, 'subjects.html', {'subjects': subjects})
+
+
+def show_teachers(request):
+    try:
+        sno = int(request.GET.get('sno'))
+        teachers = []
+        if sno:
+            subject = Subject.objects.only('name').get(no=sno)
+            teachers = Teacher.objects.filter(subject=subject).order_by('no')
+        return render(request, 'teachers.html', {
+            'subject': subject,
+            'teachers': teachers
+        })
+    except (ValueError, Subject.DoesNotExist):
+        return redirect('/')
+
+
+def praise_or_criticize(request: HttpRequest) -> HttpResponse:
+    if request.session.get('userid'):
+        try:
+            tno = int(request.GET.get('tno'))
+            teacher = Teacher.objects.get(no=tno)
+            if request.path.startswith('/praise/'):
+                teacher.good_count += 1
+                count = teacher.good_count
+            else:
+                teacher.bad_count += 1
+                count = teacher.bad_count
+            teacher.save()
+            data = {'code': 20000, 'mesg': '投票成功', 'count': count}
+        except (ValueError, Teacher.DoesNotExist):
+            data = {'code': 20001, 'mesg': '投票失败'}
+    else:
+        data = {'code': 20002, 'mesg': '请先登录'}
+    return JsonResponse(data)
+
+
+def get_captcha(request: HttpRequest) -> HttpResponse:
+    """验证码"""
+    captcha_text = gen_random_code()
+    request.session['captcha'] = captcha_text
+    image_data = Captcha.instance().generate(captcha_text)
+    return HttpResponse(image_data, content_type='image/png')
+
+
+def login(request: HttpRequest) -> HttpResponse:
+    hint = ''
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if username and password:
+            password = gen_md5_digest(password)
+            user = User.objects.filter(username=username, password=password).first()
+            if user:
+                request.session['userid'] = user.no
+                request.session['username'] = user.username
+                return redirect('/')
+            else:
+                hint = '用户名或密码错误'
+        else:
+            hint = '请输入有效的用户名和密码'
+    return render(request, 'login.html', {'hint': hint})
+
+
+def register(request: HttpRequest) -> HttpResponse:
+    hint = '填入注册信息'
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password_again = request.POST.get('password_again')
+        tel = request.POST.get('tel')
+        if all([username, password, password_again, tel]):
+            try:
+                user = User.objects.get(username=username)
+                hint = '此用户名已存在'
+                request.session.flush()
+            except User.DoesNotExist:
+                if password == password_again:
+                    md = hashlib.md5()
+                    md.update(password.encode('utf-8'))
+                    passwd = md.hexdigest()
+                    user = User(username=username, password=passwd, tel=tel)
+                    user.save()
+                    hint = ''
+                    return render(request, 'login.html', {'hint': hint})
+                else:
+                    hint = '两次输入的密码不一致'
+        else:
+            hint = '请填入全部信息'
+    return render(request, 'register.html', {'hint': hint})
+
+
+def logout(request):
+    """注销"""
+    request.session.flush()
+    return redirect('/')
+
+
+def export_teachers_excel(request):
+    #  制作工作报表
+    # 创建工作簿
+    wb = xlwt.Workbook()
+    # 添加工作表
+    sheet = wb.add_sheet('老师信息表')
+    # 查询所有老师的信息
+    queryset = Teacher.objects.all().select_related('subject')
+    # 向Excel表单中写入表头
+    colnames = ('姓名', '介绍', '好评数', '差评数', '学科')
+    for index, name in enumerate(colnames):
+        sheet.write(0, index, name)
+    # 向单元格中写入老师的数据
+    props = ('name', 'intro', 'good_count', 'bad_count', 'subject')
+    for row, teacher in enumerate(queryset):
+        for col, prop in enumerate(props):
+            value = getattr(teacher, prop, '')
+            if isinstance(value, Subject):
+                value = value.name
+            sheet.write(row + 1, col, value)
+    # 保存Excel
+    buffer = BytesIO()
+    wb.save(buffer)
+    # 将二进制数据写入响应的消息体中并设置MIME类型
+    resp = HttpResponse(buffer.getvalue(), content_type='text/csv')
+    # 中文文件名需要处理成百分号编码
+    filename = quote('老师.xls')
+    # 通过响应头告知浏览器下载该文件以及对应的文件名
+    resp['Content-Disposition'] = 'attachment; filename={}'.format(filename)
+    return resp
+
+
+def get_teachers_data(request):
+    # 生成前端图表
+    queryset = Teacher.objects.all().only('name', 'good_count', 'bad_count')
+    # 查询平均数
+    queryset1 = Teacher.objects.values('subject__name').annotate(good=Avg('good_count'), bad=Avg('bad_count'))
+    for key, value in enumerate(queryset1):
+        print(str(key) + ':' + str(value))
+    names = [teacher.name for teacher in queryset]
+    good_counts = [teacher.good_count for teacher in queryset]
+    bad_counts = [teacher.bad_count for teacher in queryset]
+    return JsonResponse({'names': names, 'good': good_counts, 'bad': bad_counts})
+
+
+def teacher_data_show(request):
+    # 返回显示数据的模板
+    return render(request, 'teacher_data.html',)
+
